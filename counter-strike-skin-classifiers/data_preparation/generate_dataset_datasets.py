c16acc2e7817e4e4d035d227ca2cbd9432257f1d
Stelios Bitzas <stelios.bitzas@gmail.com>
2024-04-15 08:54:19 +0200

    manually create quality2 dataset

--- counter-strike-skin-classifiers/data_preparation/generate_dataset_datasets.py
+++ counter-strike-skin-classifiers/data_preparation/generate_dataset_datasets.py
@@ -14,6 +14,9 @@ dataset_prices = load_dataset("imagefolder", data_dir=absolute_price_images_path
 quality_images_path = os.path.join(DATASET_BASE_PATH, "skin_images_quality_labeled", "quality_image_datasets")
 absolute_quality_images_path = os.path.abspath(quality_images_path)
 dataset_qualities = load_dataset("imagefolder", data_dir=absolute_quality_images_path)
+quality_images_path_2 = os.path.join(DATASET_BASE_PATH, "skin_images_quality_labeled", "quality_image_datasets_2")
+absolute_quality_images_path_2 = os.path.abspath(quality_images_path_2)
+dataset_qualities_2 = load_dataset("imagefolder", data_dir=absolute_quality_images_path_2)
 
 
 def save_datasets():
@@ -28,5 +31,6 @@ def save_datasets():
 if __name__ == "__main__":
     # save_datasets()
     # print("Datasets saved")
-    dataset_prices.push_to_hub("stelioshd/counter_strike_skins_prices")
-    dataset_qualities.push_to_hub("stelioshd/counter_strike_skins_quality")
+    # dataset_prices.push_to_hub("stelioshd/counter_strike_skins_prices")
+    # dataset_qualities.push_to_hub("stelioshd/counter_strike_skins_quality")
+    dataset_qualities_2.push_to_hub("stelioshd/counter_strike_skins_quality_2")
